74920e937235ec26d133496349e6709791fd44fa
Archit Singh <contact.architsingh@gmail.com>
2023-10-27 18:54:44 +0530

    new: update angular initi

--- angular-example/ui/src/app/app.module.ts
+++ angular-example/ui/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { SunbirdVideoPlayerModule} from '@project-sunbird/sunbird-video-player-v9';
 import { AppComponent } from './app.component';
-
+import { QuestionCursor } from '@project-sunbird/sunbird-quml-player-v9';
+import { QuestionCursorImplementationService } from './question-cursor-implementation.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SunbirdVideoPlayerModule
   ],
-  providers: [],
+  providers: [{ provide: QuestionCursor, useClass: QuestionCursorImplementationService }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
